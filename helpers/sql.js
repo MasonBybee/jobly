dd95b4d7250894758d36db527cbd01518f2f14ec
Mason Bybee <bybeemason@gmail.com>
2024-04-07 18:35:11 -0500

    added documentation & made jsToSql optional as seemingly intended

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,14 +1,25 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**  Creates a SQL update clause from an object.
+ 
+ dataToUpdate - contains the fields to update {key: new value}
 
-function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+ jsToSql- maps javascript field names to SQL column names
+
+ returns Object with SQL set clause setCols and values 
+
+ Example: 
+ sqlForPartialUpdate({ firstName: 'Aliya', age: 32 }, { firstName: 'first_name' })
+ returns { setCols: '"first_name"=$1, "age"=$2', values: ['Aliya', 32] }
+ */
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql = {}) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  const cols = keys.map(
+    (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
   );
 
   return {
