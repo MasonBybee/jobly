37f2d55875fc6f5475e76403063e8a57ce768296
Mason Bybee <bybeemason@gmail.com>
2024-04-07 15:52:25 -0500

    moved dblogin to .env file

--- config.js
+++ config.js
@@ -9,11 +9,13 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
 const PORT = +process.env.PORT || 3001;
 
+const DBLOGIN = process.env.DBLOGIN;
+
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "postgresql:///jobly_test"
-      : process.env.DATABASE_URL || "postgresql:///jobly";
+  return process.env.NODE_ENV === "test"
+    ? `postgresql://${DBLOGIN}/jobly_test`
+    : process.env.DATABASE_URL || `postgresql://${DBLOGIN}/jobly`;
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
