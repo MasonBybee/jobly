062a1c951eae31a395f339daa590b3f3a76c0da2
Mason Bybee <bybeemason@gmail.com>
2024-04-08 20:25:25 -0500

    changed wording

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -151,7 +151,7 @@ describe("GET /companies/:handle", function () {
 /************************************** PATCH /companies/:handle */
 
 describe("PATCH /companies/:handle", function () {
-  test("works for users", async function () {
+  test("works for admins", async function () {
     const resp = await request(app)
       .patch(`/companies/c1`)
       .send({
@@ -220,7 +220,7 @@ describe("PATCH /companies/:handle", function () {
 /************************************** DELETE /companies/:handle */
 
 describe("DELETE /companies/:handle", function () {
-  test("fails for users", async function () {
+  test("works for admins", async function () {
     const resp = await request(app)
       .delete(`/companies/c1`)
       .set("authorization", `Bearer ${u3Token}`);
